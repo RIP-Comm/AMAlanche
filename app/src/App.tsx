be5bc6945d4fe04efdfb255ccb76dc95f1451a23
papo1011 <francesco.papini.siena@gmail.com>
2022-11-22 19:45:52 +0100

    added redux

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ChakraProvider } from '@chakra-ui/react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { makeStore } from './redux/store';
 import Router from './router/root';
 
 function App() {
+  const store = makeStore();
+
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <HelmetProvider>
-          <Router />
-        </HelmetProvider>
-      </BrowserRouter>
-    </ChakraProvider>
+    <PersistGate persistor={store}>
+      <ChakraProvider>
+        <BrowserRouter>
+          <HelmetProvider>
+            <Router />
+          </HelmetProvider>
+        </BrowserRouter>
+      </ChakraProvider>
+    </PersistGate>
   );
 }
 
--- app/src/__tests__/Home.test.tsx
+++ app/src/__tests__/Home.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
-test('renders title', () => {
+test('renders title', async () => {
   render(<App />);
-  const title = screen.getByText('AMAlanche');
-  expect(title).toBeInTheDocument();
+  await waitFor(() => {
+    const title = screen.getByText('AMAlanche');
+    expect(title).toBeInTheDocument();
+  });
 });
