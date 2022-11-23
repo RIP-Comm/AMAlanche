a074f816392db8b48e1325bafc322a28a71373d6
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2022-11-23 09:26:42 +0100

    refactor: add health status api

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -4,12 +4,13 @@ import routes from '../routes';
 import { isCelebrateError } from 'celebrate';
 
 export default ({ app }: { app: express.Application }) => {
-  app.get('/status', (req: Request, res: Response) => {
-    res.status(200).end();
-  });
-  app.head('/status', (req: Request, res: Response) => {
-    res.status(200).end();
-  });
+  app.route('/health')
+    .get((req: Request, res: Response) => {
+      res.status(200).send("AMALANCHE Service: Up & Ready");
+    })
+    .head((req: Request, res: Response) => {
+      res.status(200).end();
+    });
 
   app.use(cors());
   app.use(express.json());
