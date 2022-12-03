1e9f3dd9c22a5bd5347f20d8ee25bca6aa7d72cc
Simone Golinucci <s.golinucci@reply.it>
2022-12-03 11:36:32 +0100

    fix(mongo):fix mongo check properties in dedicated loader

--- src/loaders/mongo.ts
+++ src/loaders/mongo.ts
@@ -4,7 +4,12 @@ import Logger from './logger';
 export default async (): Promise<typeof mongoose> => {
   //add here mongo DB configuration
   let uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
-  if (process.env.MONGO_PASSWORD != null && process.env.MONGO_PASSWORD != null) {
+  if (
+    process.env.MONGO_PASSWORD &&
+    process.env.MONGO_PASSWORD != '' &&
+    process.env.MONGO_USERNAME &&
+    process.env.MONGO_USERNAME != ''
+  ) {
     uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
   }
   Logger.info(`Connection uri: ${uri}`);
